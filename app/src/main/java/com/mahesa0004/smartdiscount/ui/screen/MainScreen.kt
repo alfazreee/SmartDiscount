e819c43cd0d98fbf0c0c9a583229ef473aa845a8
Mahesa Revanenda Al Fajri <alfajrief@gmail.com>
2026-04-08 10:03:13 +0700

    Meminta input dari pengguna dan membuat perhitungan diskon

--- app/src/main/java/com/mahesa0004/smartdiscount/ui/screen/MainScreen.kt
+++ app/src/main/java/com/mahesa0004/smartdiscount/ui/screen/MainScreen.kt
@@ -4,16 +4,25 @@ import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -23,6 +32,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mahesa0004.smartdiscount.R
 import com.mahesa0004.smartdiscount.ui.theme.SmartDiscountTheme
+import androidx.compose.material3.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,11 +55,21 @@ fun MainScreen() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
+
+    var harga by remember { mutableStateOf("") }
+    var diskon by remember { mutableStateOf("") }
+    var isPersen by remember { mutableStateOf(true) }
+    var hasil by remember { mutableStateOf(0.0) }
+    var expanded by remember { mutableStateOf(false) }
+    val options = listOf("%", "Rp")
+    var selectOption by remember { mutableStateOf(options[0]) }
+
     Column(
         modifier = modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Top,
+        verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Image(
@@ -57,6 +77,78 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             contentDescription = stringResource(R.string.logo),
             contentScale = ContentScale.Fit,
             modifier = Modifier.size(132.dp)
+        )
+        Text(
+            text = stringResource(R.string.app_Intro)
+        )
+        OutlinedTextField(
+            value = harga,
+            onValueChange = {harga = it},
+            label = {Text(stringResource(R.string.harga)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            OutlinedTextField(
+                value = diskon,
+                onValueChange = {diskon = it},
+                label = {Text(stringResource(R.string.diskon)) },
+                modifier = Modifier.weight(1f)
+            )
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = {expanded = !expanded}
+            ) {
+                OutlinedTextField(
+                    value = selectOption,
+                    onValueChange = {},
+                    readOnly = true,
+                    label = {Text("Tipe") },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded)
+                    },
+                    modifier = Modifier.width(90.dp).menuAnchor()
+                )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = {expanded = false}
+            ) {
+                options.forEach { options ->
+                    DropdownMenuItem(
+                        text = {Text(options)},
+                        onClick = {
+                            selectOption = options
+                            isPersen = options == "%"
+                            expanded = false
+                            }
+                        )
+                    }
+                }
+            }
+        }
+        Button(
+            onClick = {
+                val hargaValue = harga.toDoubleOrNull() ?: 0.0
+                val diskonValue = diskon.toDoubleOrNull() ?:0.0
+
+                hasil = if (isPersen) {
+                    hargaValue - (hargaValue * diskonValue / 100)
+                } else {
+                    hargaValue - diskonValue
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.hitung))
+        }
+        Text(stringResource(R.string.hasil))
+
+        Text(
+            text = "${stringResource(R.string.harga_akhir)}: Rp ${hasil.toInt()}",
+            style = MaterialTheme.typography.titleLarge
         )
     }
 }
